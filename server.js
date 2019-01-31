605c21114bdc7cfc1671dee1d2870ba8c9a014c4
Zhe WU <you@example.com>
2019-01-31 17:37:00 +0100

    change name again

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/myCine'));
+app.use(express.static('./dist/my-cine'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/myCine/index.html'));
+res.sendFile(path.join(__dirname,'/dist/my-cinee/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
